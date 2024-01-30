574dca66ac6477b3a42ff80d9bcdc31b6b128f55
Fidger <fidgergangofficial@mail.ru>
2024-01-31 01:18:06 +0300

    commit5

--- task_07.py
+++ task_07.py
@@ -1,6 +1,7 @@
 def combine_anagrams(words_array):
     dictionary = {}
-    wrds = [''.join(sorted(list(x))) for x in words_array]
+    wrds = [''.join(sorted(x)) for x in words_array]
+    print(wrds)
     for index, value in enumerate(wrds):
         if value in dictionary.keys():
             dictionary[value].append(index)
@@ -13,3 +14,7 @@ def combine_anagrams(words_array):
             a.append(words_array[index])
         res.append(a)
     return res
+
+
+print(combine_anagrams(["cars", "for", "potatoes", "racs", "four", "scar",
+"creams", "scream"]))
